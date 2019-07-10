e51a4cc9176053d29f9d95da16a353f6509b35cb
astersetu99 <52713286+astersetu99@users.noreply.github.com>
2019-07-10 20:17:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,14 @@ var bot = new Discord.Client();
 bot.on('message', message =>{
 
     // Variables
-    var sender = message.author; //The person who sent the message
-    var msg = message.content;
-    var prefix = ">" //The text before commands, you can put anything that you prefer
-
-    bot = commands.Bot(command_prefix='tf')
+   if(message.author.id != "598197648167534632" && message.channel.id === "597329293533511691"){
+        if(msg.startsWith('>', 0)){
+            if(msg === prefix + "tf" && message.channel.id === "597329293533511691"){
 }
 @bot.event
 async def on_message(message):
     if(message.channel.id == "597329293533511691"):
-        await bot.add_reaction(message, "discord_emote_id_here")
+        await bot.add_reaction(message, "\:white_check_mark: ")
             }
             
         }
