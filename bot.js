ec0c06c803e6ead5d9dd648ef8f8e5876cadd02e
astersetu99 <52713286+astersetu99@users.noreply.github.com>
2019-07-10 20:34:30 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = ">" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "598197648167534632" && message.channel.id === "597329293533511691"){
+        if(msg.startsWith('>', 0)){
+            if(msg === prefix + "tf" && message.channel.id === "597329293533511691"){
+                message.channel.send('@here **Game Is About To Start...Please React Below.....!fruits'**') // Sends a message to the channel, with the content
+            }
+            
+        }
+    }
+});
+bot.login(process.env.BOT_TOKEN)
