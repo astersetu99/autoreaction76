b0427819522c1f6294a43cd2a03c43d449d14543
astersetu99 <52713286+astersetu99@users.noreply.github.com>
2019-07-10 19:52:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "597802520601034790" && message.channel.id === "595640732564586543","595635734904307742","595636121124208640","595639586726740049","595639853962756110"){
+    if(message.author.id != "598197648167534632" && message.channel.id === "597329293533511691"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "tf" && message.channel.id === "595640732564586543","595635734904307742","595636121124208640","595639586726740049","595639853962756110"){
+            if(msg === prefix + "tf" && message.channel.id === "597329293533511691"){
                 message.channel.send('@here **Game Is About To Start...Please React Below.....**') // Sends a message to the channel, with the content
             }
             
