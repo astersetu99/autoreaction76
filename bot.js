15629e5b989d7a91547a3913ece8e62fce803f58
astersetu99 <52713286+astersetu99@users.noreply.github.com>
2019-07-10 19:57:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,12 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "598197648167534632" && message.channel.id === "597329293533511691"){
-        if(msg.startsWith('>', 0)){
-            if(msg === prefix + "tf" && message.channel.id === "597329293533511691"){
-                message.channel.send('@here **Game Is About To Start...Please React Below.....**') // Sends a message to the channel, with the content
+    bot = commands.Bot(command_prefix='whatever_prefix_u_want_here')
+}
+@bot.event
+async def on_message(message):
+    if(message.channel.id == "597329293533511691"):
+        await bot.add_reaction(message, "discord_emote_id_here")
             }
             
         }
